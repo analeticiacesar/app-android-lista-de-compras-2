f7d7a0783afe29fa517d2b8cd03081c249d63b64
Ana Letícia César de Oliveira <analeticiacesardeoliveira@gmail.com>
2023-07-01 20:22:37 -0300

    feature: creates activity_main layout file.

--- app/src/main/java/com/analeticiacesar/listadecompras/MainActivity.kt
+++ app/src/main/java/com/analeticiacesar/listadecompras/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.analeticiacesar.listadecompras
 
 import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
 
 class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+    }
 }

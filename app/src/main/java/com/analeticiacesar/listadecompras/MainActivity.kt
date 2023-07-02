6aa6bd2d29b3067de2df4975f457ea9e19415f9c
Ana Letícia César de Oliveira <analeticiacesardeoliveira@gmail.com>
2023-07-02 00:30:41 -0300

    feature: creates item layout file.

--- app/src/main/java/com/analeticiacesar/listadecompras/MainActivity.kt
+++ app/src/main/java/com/analeticiacesar/listadecompras/MainActivity.kt
@@ -3,10 +3,13 @@ package com.analeticiacesar.listadecompras
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import com.analeticiacesar.listadecompras.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
